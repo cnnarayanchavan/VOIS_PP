69741a737f09a542620f05eaf766eeb66c105e72
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-04-27 12:26:33 +0530

    Update Selection.java_CODE

--- Selection.java
+++ Selection.java
@@ -1,1 +1,24 @@
-
+//Suppose array is [5,4,3,2,1]
+//Selection Sort
+import java.util.*;
+import java.util.Arrays;
+public class Selection{
+  public static void main(String[] args){
+    int array = {5,4,3,2,1};
+    int op = selectElement(array);
+    System.out.print(Arrays.toString(op));
+  }
+  public static int selectElement(int arr){
+    for(int i = 0; i<n-2; i++){
+      int mini = i;
+      for(int j = 0; j<n-1; j++){
+        if(arr[j]<arr[mini]){
+         //mini = j;
+          int temp = arr[mini];
+          arr[mini] = arr[j];
+          arr[j] = temp;
+        }
+      }
+    }
+  }
+}
