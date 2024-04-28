422f95db461f4fd994edf6c797aefd83af3df8b3
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-04-28 23:50:06 +0530

    Successfullt coded

--- Insertion.java
+++ Insertion.java
@@ -1,16 +1,16 @@
 import java.util.Arrays;
 public class Insertion{
   public static void main(String[]args){
-    int arr = {5,3,4,2,1};
+    int[] arr = {56,3,53,34,6,4}; //{5,3,4,2,1};
     for(int i = 0; i < arr.length-1; i++){
       for(int j = i+1; j>0; j--){
         if(arr[j]<arr[j-1]){
-          int temp = int[j];
-          int[j] = arr[j-1];
+          int temp = arr[j];
+          arr[j] = arr[j-1];
           arr[j-1] = temp;
         }
       }
     }
-    System.out.print(Array.to String(arr))
+    System.out.print(Arrays.toString(arr));
   }
 }
