58fe9d9801fcc4a394a3f6a9763434d59b10fa44
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-04-29 22:33:00 +0530

    Update Tcs1st.java

--- Tcs1st.java
+++ Tcs1st.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
+public class MyClass{
+  public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    int Number  = sc.nextInt();
+    int op = tableSum(Number);
+  }
 
+  public static int tableSum(int num){
+    int sum = 0;
+    for(int i = 0; i<=10; i++){
+      int asn = num * i;
+      sum += ans;
+    }
+    return sum;
+  }
+}
